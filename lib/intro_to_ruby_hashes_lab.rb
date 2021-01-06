be56dec4a59ec54f1bf01724b1697f3fcf9ea6b5
Devante Lowery <dlowery2120@gmail.com>
2021-01-06 04:35:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,11 +11,13 @@ end
 
 def pioneer
   hash = {
-    :
+    :name => "Grace Hopper"
   }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  number = {
+    :id => 2
+  }
 end
